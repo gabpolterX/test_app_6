708d7e0030645f418c7d3bf537faf1db1ebe7e6d
Gabriel Castro <gabocastrosemper28@gmail.com>
2024-12-16 04:59:41 -0500

    Finished many-to-many associations

---
+++ app/models/category.rb
@@ -0,0 +1,8 @@
+class Category < ApplicationRecord
+    has_many :article_categories
+    has_many :articles, through: :article_categories
+    
+    validates :name, presence: true, uniqueness: true, length: { minimum: 4, maximum: 25 }
+
+
+end
